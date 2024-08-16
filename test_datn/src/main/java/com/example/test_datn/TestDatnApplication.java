d06236bac217b02b9065c17205d9150cc6459c62
namtp04 <namtran14022004@gmail.com>
2024-08-16 14:41:38 +0700

    ok

---
+++ test_datn/src/main/java/com/example/test_datn/TestDatnApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDatnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDatnApplication.class, args);
+	}
+
+}
